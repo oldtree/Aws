9a404e1b3561178896cb03cdea19debc3463fdf1
Grapes <435594427@qq.com>
2015-04-30 18:02:02 +0800

    add watcher

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 	//Default will using on dev model
 	r := gin.Default()
 
-	//in busness model wll not print any thing ,just out put to the log file
 	blog := r.Group("/blog")
 	{
 		blog.GET("", handlers.HandleMain)
-		blog.GET("/about", handlers.HandleAbout)
+		blog.GET("/history", handlers.HandleHistory)
+		blog.GET("/watcher", handlers.HandleWatcher)
 	}
 
 	r.Run(":8888")
